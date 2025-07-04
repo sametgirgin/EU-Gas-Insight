3a7513beacab8ab620e10853e5d6a9c7a8991caf
SAMET GİRGİN <44918645+sametgirgin@users.noreply.github.com>
2025-07-04 11:41:46 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -24,45 +24,6 @@ with left_col:
 with right_col:
     # Display the logo
     st.image("logo.png", width=30)  # Adjust the width as needed
-# Configure the Gemini API with your API key from secrets
-GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
-
-if not GOOGLE_API_KEY:
-    st.error("API Key not found. Please set the GOOGLE_API_KEY in secrets.toml.")
-    st.stop()
-
-genai.configure(api_key=GOOGLE_API_KEY)
-
-# Available models (you can choose others, like 'gemini-1.5-pro-latest')
-MODEL_NAME = 'gemini-2.0-flash'
-generation_config = genai.GenerationConfig(
-    temperature=0.7,
-    top_p=1.0,
-    top_k=1,
-    max_output_tokens=2048,
-)
-safety_settings = [
-    {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-]
-
-model = genai.GenerativeModel(model_name=MODEL_NAME,
-                              generation_config=generation_config,
-                              safety_settings=safety_settings)
 
 @st.cache_data
 def load_gaspowerplants_data():
